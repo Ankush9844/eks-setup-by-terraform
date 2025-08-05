a920991efc41195530204e564f905280aadc6891
Ankush984 <ankush.katkurwar30@gmail.com>
2025-08-05 18:15:24 +0530

    seperate IAM module

--- EKS-Setup/main.tf
+++ EKS-Setup/main.tf
@@ -23,31 +23,25 @@ module "ec2_instance" {
 
 }
 
-module "iam" {
-  source       = "../modules/4-IAM"
-  project_name = var.project_name
-}
 
 module "eks_cluster" {
-  source                 = "../modules/5-EKS"
-  region                 = var.region
-  aws_profile            = var.aws_profile
-  project_name           = var.project_name
-  instance_types         = var.instance_types
-  node_group_name        = var.node_group_name
-  private_subnet_ids     = module.vpc.privateSubnetIDs
-  eks_cluster_role_arn   = module.iam.eks_cluster_role_arn
-  eks_nodegroup_role_arn = module.iam.eks_nodegroup_role_arn
+  source             = "../modules/4-EKS"
+  region             = var.region
+  aws_profile        = var.aws_profile
+  project_name       = var.project_name
+  instance_types     = var.instance_types
+  node_group_name    = var.node_group_name
+  private_subnet_ids = module.vpc.privateSubnetIDs
 }
 
 module "oidc_provider" {
-  source       = "../modules/6-OIDC"
+  source       = "../modules/5-OIDC"
   cluster_name = module.eks_cluster.eks_cluster_name
   depends_on   = [module.eks_cluster]
 }
 
 module "eks_addon" {
-  source                              = "../modules/7-ADDONS"
+  source                              = "../modules/6-ADDONS"
   cluster_name                        = module.eks_cluster.eks_cluster_name
   aws_iam_openid_connect_provider_arn = module.oidc_provider.aws_iam_openid_connect_provider_arn
   aws_iam_openid_connect_provider_url = module.oidc_provider.aws_iam_openid_connect_provider_url
@@ -55,21 +49,21 @@ module "eks_addon" {
 }
 
 module "karpenter" {
-  source                              = "../modules/8-KARPENTER"
+  source                              = "../modules/7-KARPENTER"
   region                              = var.region
   aws_profile                         = var.aws_profile
   project_name                        = var.project_name
   eks_cluster_name                    = module.eks_cluster.eks_cluster_name
   eks_cluster_endpoint                = module.eks_cluster.eks_cluster_endpoint
   cluster_ca_certificate              = module.oidc_provider.cluster_ca_certificate
-  eks_nodegroup_role_name             = module.iam.eks_nodegroup_role_name
+  eks_nodegroup_role_name             = module.eks_cluster.eks_nodegroup_role_name
   aws_iam_openid_connect_provider_arn = module.oidc_provider.aws_iam_openid_connect_provider_arn
   aws_iam_openid_connect_provider_url = module.oidc_provider.aws_iam_openid_connect_provider_url
   depends_on                          = [module.eks_cluster]
 }
 
 module "load_balancer_controller" {
-  source                              = "../modules/9-LB"
+  source                              = "../modules/8-LB"
   vpc_id                              = module.vpc.vpc_id
   aws_region                          = var.region
   cluster_name                        = module.eks_cluster.eks_cluster_name
